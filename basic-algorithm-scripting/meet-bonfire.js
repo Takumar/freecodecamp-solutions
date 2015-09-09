f80ced9410c81e15ee6985ec08aec4675de41a35
Takumar <gebi@poczta.fm>
2015-09-09 14:47:43 +0200

    Function meetBonfire() corrected.

---
+++ basic-algorithm-scripting/meet-bonfire.js
@@ -0,0 +1,36 @@
+function meetBonfire(argument) {
+  // Good luck!
+  console.log("you can read this function's argument in the developer tools", argument);
+
+  return true;
+}
+
+meetBonfire("You can do this!");
+
+
+/*
+*** 	EXERCISE DESCRIPTION	***
+
+Bonfire: Meet Bonfire
+
+
+Your goal is to fix the failing test.
+
+First, run all the tests by clicking "Run code" or by pressing Control + Enter.
+
+The failing test is in red. Fix the code so that all tests pass. Then you can move on to the next Bonfire.
+
+Make this function return true no matter what.
+
+// ***********************************************
+function meetBonfire(argument) {
+  // Good luck!
+  console.log("you can read this function's argument in the developer tools", argument);
+
+  return false;
+}
+
+meetBonfire("You can do this!");
+// ***********************************************
+
+*/
