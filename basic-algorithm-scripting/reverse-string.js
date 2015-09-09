90348271764bf9cf6ffed4bf8788205e9fcedab8
Takumar <gebi@poczta.fm>
2015-09-09 14:56:06 +0200

    Function reverseString() created.

---
+++ basic-algorithm-scripting/reverse-string.js
@@ -0,0 +1,43 @@
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString('hello');
+reverseString('Howdy');
+reverseString('Greetings from Earth');
+
+/*
+*** 	EXERCISE DESCRIPTION	***
+
+Bonfire: Reverse a String
+
+
+Reverse the provided string.
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+    Global String Object 
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String">link</a>
+
+    String.split() 
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split">link</a>
+    Array.reverse()
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse">link</a>
+    Array.join()
+<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join">link</a>
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+// ***********************************************
+function reverseString(str) {
+  return str;
+}
+
+reverseString('hello');
+// ***********************************************
+
+*/
