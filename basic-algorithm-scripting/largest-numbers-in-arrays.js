52b8143e04c79ec97953bee122a91dc6335a893e
Takumar <gebi@poczta.fm>
2015-09-17 17:50:16 +0200

    'Return Largest Numbers in Arrays' bonfire solved (11.08.2015)

---
+++ basic-algorithm-scripting/largest-numbers-in-arrays.js
@@ -0,0 +1,55 @@
+function largestOfFour(arr) {
+  var resultArr = [];
+  var max_value = 0;
+  var current_value = 0;
+  // first we loop through sub arrays
+  for (var ind_y = 0; ind_y < arr.length; ind_y++) {
+    // we initialize variable max_value which represents maximum value in sub array
+    max_value = arr[ind_y][0];
+    // next we iterate through elements of sub array
+    for (var ind_x = 1; ind_x < 4; ind_x++) {
+      current_value = arr[ind_y][ind_x];
+      if (current_value >= max_value) {
+        max_value = current_value;
+      }
+    }
+    resultArr[ind_y] = max_value;
+  }
+  
+  return resultArr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+/*
+*** 	EXERCISE DESCRIPTION	***
+
+Bonfire: Return Largest Numbers in Arrays
+Difficulty: 
+
+Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i] .
+
+If you are writing your own Chai.js tests, be sure to use a deep equal statement instead of an equal statement when comparing arrays.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+    Comparison Operators
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comparison_Operators
+	
+// ***********************************************
+
+function largestOfFour(arr) {
+  // You can do this!
+  return arr;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]], "");
+
+// ***********************************************
+
+*/
