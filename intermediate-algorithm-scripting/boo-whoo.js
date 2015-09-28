4f807c20ace638ee768cced86ba1e114f58c3c01
Takumar <gebi@poczta.fm>
2015-09-28 23:24:40 +0200

    'Boo who' bonfire solved. 25.09.2015

---
+++ intermediate-algorithm-scripting/boo-whoo.js
@@ -0,0 +1,38 @@
+function boo(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (bool === true || bool === false) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+boo(null);
+
+
+/*
+*** 	EXERCISE DESCRIPTION	***
+Bonfire: Boo who
+Difficulty: 
+
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+Here are some helpful links:
+
+    Boolean Objects
+	https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean
+
+// ***********************************************
+function boo(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return bool;
+}
+
+boo(null);
+// ***********************************************
+*/
