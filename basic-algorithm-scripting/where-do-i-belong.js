28c3f0e1cb20343109a6f70243d63b0dfd2f31ee
Takumar <gebi@poczta.fm>
2015-11-13 14:07:22 +0100

    'Where do I belong' bonfire correct to new scenario

--- basic-algorithm-scripting/where-do-i-belong.js
+++ basic-algorithm-scripting/where-do-i-belong.js
@@ -1,24 +1,26 @@
 function where(arr, num) {
   // Find my place in this sorted array.
+  arr.sort(function(a, b) { return a-b; });
+
   for (var index=0; index < arr.length; index++) {
     if (arr[index] >= num) {
-      return index;
-    };
-  };
-
+      break;
+    }
+  }
+  return index;
 }
 
 where([40, 60], 50);
+
 
 /*
 *** 	EXERCISE DESCRIPTION	***
 Bonfire: Where do I belong
 Difficulty: 
 
-Return the lowest index at which a value (second argument) should be inserted into a sorted array (first argument).
-
-For example, where([1,2,3,4], 1.5) should return 1 because it is greater than 1 (0th index), but less than 2 (1st index).
-
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted.
+For example, where([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+Likewise, where([20,3,5], 19) should return 2 because it is less than 20 (index 2) and greater than 5 (index 1).
 Remember to use Read-Search-Ask if you get stuck. Write your own code.
 
 Here are some helpful links:
