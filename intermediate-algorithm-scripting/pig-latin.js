b943dc5e2aa2abad0ce3eb151ccfc8a6f378e922
Aleksander Gębicki <gebi@poczta.fm>
2016-01-05 21:22:41 +0100

    'Pig Latin' bonfire solved 5.01.2016

---
+++ intermediate-algorithm-scripting/pig-latin.js
@@ -0,0 +1,28 @@
+function translate(str) {
+  var regexp = /[aeiou]/;
+  var vowelIdx = str.search(regexp);
+
+  if (vowelIdx <= 0) {
+    return str + "way";
+  }
+  else {
+    return str.substr(vowelIdx) + str.substr(0, vowelIdx) + "ay";
+  }
+}
+
+translate("consonant");
+translate("paragraphs"); //should return "aragraphspay".translate("paragraphs")
+translate("glove"); //should return "oveglay".
+translate("algorithm"); //should return "algorithmway".
+translate("eight"); // should return "eightway".
+
+/*
+*** 	EXERCISE DESCRIPTION	***
+Bonfire: Pig Latin
+Translate the provided string to pig latin.
+Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+If a word begins with a vowel you just add "way" to the end.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+*/
