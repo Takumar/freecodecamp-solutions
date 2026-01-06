6631fbd458a53e08be50eb36b17987f5682e426a
Aleksander Gębicki <gebi@poczta.fm>
2026-01-06 23:49:36 +0100

    validate cipher parameters

---
+++ python_certification/caesar_cipher.py
@@ -0,0 +1,16 @@
+def caesar(text, shift):
+
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+    
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    return text.translate(translation_table)
+
+
+encrypted_text = caesar('freeCodeCamp', 3)
+print(encrypted_text)
