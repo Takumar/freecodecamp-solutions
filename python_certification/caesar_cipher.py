719215b1ea0990fe5876aa236947ca53015d617b
Aleksander Gębicki <gebi@poczta.fm>
2026-01-07 00:02:11 +0100

    Add encrypt, decrypt and validation functions

--- python_certification/caesar_cipher.py
+++ python_certification/caesar_cipher.py
@@ -1,16 +1,27 @@
-def caesar(text, shift):
+def caesar(text, shift, encrypt=True):
 
     if not isinstance(shift, int):
         return 'Shift must be an integer value.'
 
     if shift < 1 or shift > 25:
         return 'Shift must be an integer between 1 and 25.'
-    
+
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
+    
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
-    return text.translate(translation_table)
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
 
-
-encrypted_text = caesar('freeCodeCamp', 3)
-print(encrypted_text)
+def encrypt(text, shift):
+    return caesar(text, shift)
+    
+def decrypt(text, shift):
+    return caesar(text, shift, encrypt=False)
+    
+encrypted_text = "Pbhentr vf sbhaq va hayvxryl cynprf."
+decrypted_text = decrypt(encrypted_text, 13)
+print(decrypted_text)
